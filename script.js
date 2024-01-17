1eddb973fc18558b09c5b2ec76022aecc505f943
Abdelrahman Hassan <139147461+valorion01@users.noreply.github.com>
2024-01-17 15:11:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Array of sample deals
     const dealsData = [
-        { name: "Utopia Bedding Queen Bed Sheets Set(Queen, Grey)", image: "bedsheet.jpg", link: "https://amzn.to/48BBYma" },
         { name: "Wireless Headphones", image: "wireless-headphones.jpg", link: "https://www.plaisio.gr/gaming-zone/aksesouar-gia-konsoles/sony-gold-black-wireless-headset_2873842?qId=6640455434af0fb3a4ed162b51533a7d&qIx=products" },
-        { name: "4K Ultra HD TV", image: "4k-tv.jpg", link: "https://example.com/4k-tv" },
+        { name: "Mielle Organics Rosemary Oil", image: "rosemary oil.jpg", link: "https://amzn.to/4b2AFhu" },
         { name: "Running Shoes", image: "running-shoes.jpg", link: "https://example.com/running-shoes" },
         { name: "Home Security Camera", image: "security-camera.jpg", link: "https://example.com/security-camera" },
         { name: "Samsung 4K Monitor", image: "samsung-monitor.jpg", link: "https://www.amazon.com/SAMSUNG-U28R550UQNX-LU28R550UQNXZA-Monitor-Free/dp/B084V9CJB1?ref_=Oct_DLandingS_D_860e167d_0&th=1" },
+        { name: "Utopia Bedding Queen Bed Sheets Set(Queen, Grey)", image: "bedsheet.jpg", link: "https://amzn.to/48BBYma" },                  
     ];
 
     // Display the deals on page load
